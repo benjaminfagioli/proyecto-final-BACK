6c38ff9caa2b58928971a224d0d2863937f8dc56
Bruno Tornese <brunoluchi2003@gmail.com>
2024-03-04 15:59:19 -0300

    Agregado del rol a users

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,14 +3,15 @@ import { Schema, model } from "mongoose";
 const newUser = new Schema(
   {
     name: String,
-
     email: String,
-
     password: String,
-
     isActive: {
       type: Boolean,
       default: true,
+    },
+    role: {
+      type: String,
+      default: "user",
     },
   },
   {
