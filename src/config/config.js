1ca6c9df2dedf1b7bf2534ab9f854f7318f24f5c
benjaminfagioli <benjitafagg@gmail.com>
2024-03-15 00:14:17 -0300

    cambios en el login y config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,3 +4,5 @@ dotenv.config();
 export const CONNECTION_STRING = process.env.CONNECTION_STRING;
 export const PORT = process.env.PORT;
 export const SECRET = process.env.SECRET;
+export const ADMIN_KEY = process.env.ADMIN_KEY;
+export const USER_KEY = process.env.USER_KEY;
